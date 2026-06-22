dc37628220a23f9b4cfab59ac92d375f0a42a4c3
Muhammad Ameen <muhammedameen9655@gmail.com>
2026-06-22 15:24:19 +0530

    added JK

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 vim.opt.clipboard = "unnamedplus"
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "JK", "<Esc>", { noremap = false })
 
 vim.g.mapleader = " "
 
